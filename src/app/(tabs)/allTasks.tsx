f63f06ed1136ccb0fa029e68909a6a01d88b685b
webowski <okwebowski@gmail.com>
2026-04-27 14:06:02 +0300

    почистил код allTask.tsx

--- src/app/(tabs)/allTasks.tsx
+++ src/app/(tabs)/allTasks.tsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import { ActivityIndicator, Text, TextInput, View } from 'react-native'
 
 import { useTasks } from '@/features/TaskList/model'
+import TaskListItem from '@/features/TaskList/TaskListItem'
 import ScrollBox from '@/shared/ui/ScrollBox'
 
 export default function AllTasksScreen() {
 	const [searchQuery, setSearchQuery] = useState('')
 
-	const { data, isLoading, error } = useTasks({
+	const {
+		data: tasks,
+		isLoading,
+		error
+	} = useTasks({
 		searchQuery: searchQuery || undefined
 	})
 
@@ -24,15 +29,8 @@ export default function AllTasksScreen() {
 					style={{ flex: 1, borderWidth: 1, padding: 8 }}
 				/>
 
-				{data?.map((task, taskIndex) => (
-					<View key={task.id}>
-						<Text>{task.info}</Text>
-						{/* <FlatList
-							data={task.data}
-							keyExtractor={(item, index) => 'taskIndex' + '-' + index}
-							renderItem={({ item }) => <TaskListItem data={item} />}
-						/> */}
-					</View>
+				{tasks?.map((task) => (
+					<TaskListItem key={task.id} data={task} />
 				))}
 			</View>
 		</ScrollBox>
