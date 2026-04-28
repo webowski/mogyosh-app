e3078acc8a485c7517595f9c341b62aa51ef6a64
webowski <okwebowski@gmail.com>
2026-04-28 07:41:05 +0300

    удалил MindMap.web.tsx

--- src/features/MindMap/__MindMap.web.tsx
+++
@@ -1,9 +0,0 @@
-// src/features/MindMap/MindMap.web.tsx
-import React from 'react'
-
-import NativeMindMap from './MindMap' // импортируем нативную версию
-
-export default function MindMap() {
-	// Skia уже инициализирован в index.web.tsx
-	return <NativeMindMap />
-}
