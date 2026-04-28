e9d0f505ea590dfd9dafce7475d837a0547ac53e
webowski <okwebowski@gmail.com>
2026-04-28 10:15:32 +0300

    feat(mindmap): add useCanvasFont hook for platform-specific fonts

---
+++ src/features/MindMap/useСanvasFont.ts
@@ -0,0 +1,25 @@
+import { DataSourceParam, matchFont, useFont } from '@shopify/react-native-skia'
+import { Platform } from 'react-native'
+
+export function useCanvasFont(fontSize: number) {
+	const nativeFont =
+		Platform.OS !== 'web'
+			? matchFont({
+					fontFamily: Platform.select({
+						ios: 'Helvetica Neue',
+						android: 'sans-serif'
+					})!,
+					fontSize,
+					fontWeight: 'normal'
+				})
+			: null
+
+	const webFontSource: DataSourceParam =
+		Platform.OS === 'web'
+			? { uri: 'https://fonts.gstatic.com/s/roboto/v32/KFOmCnqEu92Fr1Me5Q.ttf' }
+			: null
+
+	const webFont = useFont(webFontSource, fontSize)
+
+	return Platform.OS === 'web' ? webFont : nativeFont
+}
