a7fb16c855bbe7795259f17bc1a582744e5d8ae6
webowski <okwebowski@gmail.com>
2026-05-04 13:14:46 +0300

    отдельный файл стилей для pill

---
+++ src/shared/styles/pills.ts
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native-unistyles'
+
+import { STYLE_VARS } from './common'
+
+export const pillStyles = StyleSheet.create((theme, rt) => ({
+	pills: {
+		flexDirection: 'row',
+		gap: 8,
+		paddingHorizontal: STYLE_VARS.sidePadding,
+		backgroundColor: 'black'
+	},
+	pill: {
+		backgroundColor: theme.colors.primary800,
+		paddingVertical: 5,
+		paddingHorizontal: 14,
+		borderRadius: 20,
+		borderWidth: 1,
+		borderColor: theme.colors.primary
+	},
+	pill__active: {
+		color: theme.colors.inverse,
+		backgroundColor: theme.colors.primary
+	},
+	pill__text: {
+		fontSize: 14,
+		fontWeight: 500,
+		color: theme.colors.major
+	},
+	pill__text_active: {
+		color: theme.colors.inverse
+	}
+}))
