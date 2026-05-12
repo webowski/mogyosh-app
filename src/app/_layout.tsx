f4df0bb9d97b55f69ec2b0ecfe3516f3c2453fb1
webowski <okwebowski@gmail.com>
2026-05-12 18:32:44 +0300

    добавил BottomSheetModalProvider, и герерально включил ReduceMotion.Never для reanimated

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
@@ -5,7 +6,7 @@ import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { ActivityIndicator, Text } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
-import 'react-native-reanimated'
+import { ReducedMotionConfig, ReduceMotion } from 'react-native-reanimated'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { useUnistyles } from 'react-native-unistyles'
 
@@ -53,55 +54,58 @@ export default function RootLayout() {
 		<QueryClientProvider client={queryClient}>
 			<GestureHandlerRootView style={{ flex: 1 }}>
 				<SafeAreaProvider>
-					<StatusBar style={theme.statusBarColor} />
-					<Stack
-						screenOptions={{
-							contentStyle: {
-								backgroundColor: theme.colors.backgroundAlter
-							}
-						}}
-					>
-						<Stack.Screen
-							name='(tabs)'
-							options={{
-								headerShown: false
+					<ReducedMotionConfig mode={ReduceMotion.Never} />
+					<BottomSheetModalProvider>
+						<StatusBar style={theme.statusBarColor} />
+						<Stack
+							screenOptions={{
+								contentStyle: {
+									backgroundColor: theme.colors.backgroundAlter
+								}
 							}}
-						/>
-						<Stack.Screen
-							name='createTask'
-							options={{
-								title: t('screen.Create Task'),
-								// headerShown: false,
-								presentation: 'formSheet',
-								sheetAllowedDetents: [0.9],
-								sheetElevation: 1,
-								sheetCornerRadius: 16
-								// animation: 'slide_from_bottom'
-							}}
-						/>
-						<Stack.Screen
-							name='about'
-							options={{
-								title: t('screen.About'),
-								headerShown: true,
-								header: (props) => <Header {...props} />
-							}}
-						/>
-						<Stack.Screen
-							name='account'
-							options={{
-								title: t('screen.Account'),
-								headerShown: true,
-								header: (props) => <Header {...props} />
-							}}
-						/>
-						<Stack.Screen
-							name='settings'
-							options={{
-								headerShown: false
-							}}
-						/>
-					</Stack>
+						>
+							<Stack.Screen
+								name='(tabs)'
+								options={{
+									headerShown: false
+								}}
+							/>
+							<Stack.Screen
+								name='createTask'
+								options={{
+									title: t('screen.Create Task'),
+									// headerShown: false,
+									presentation: 'formSheet',
+									sheetAllowedDetents: [0.9],
+									sheetElevation: 1,
+									sheetCornerRadius: 16
+									// animation: 'slide_from_bottom'
+								}}
+							/>
+							<Stack.Screen
+								name='about'
+								options={{
+									title: t('screen.About'),
+									headerShown: true,
+									header: (props) => <Header {...props} />
+								}}
+							/>
+							<Stack.Screen
+								name='account'
+								options={{
+									title: t('screen.Account'),
+									headerShown: true,
+									header: (props) => <Header {...props} />
+								}}
+							/>
+							<Stack.Screen
+								name='settings'
+								options={{
+									headerShown: false
+								}}
+							/>
+						</Stack>
+					</BottomSheetModalProvider>
 				</SafeAreaProvider>
 			</GestureHandlerRootView>
 		</QueryClientProvider>
