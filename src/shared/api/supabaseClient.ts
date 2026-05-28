de7830967f7d0ee1fcc16e5871e228a342ba9513
webowski <okwebowski@gmail.com>
2026-05-28 23:06:35 +0300

    переименовал env key переменную

--- src/shared/api/supabaseClient.ts
+++ src/shared/api/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 import { createMMKV } from 'react-native-mmkv'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
 
 if (!supabaseUrl || !supabaseAnonKey) {
 	throw new Error(
