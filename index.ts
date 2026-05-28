85d21057889365a0551c1b4281997b89fd00b4c2
webowski <okwebowski@gmail.com>
2026-05-28 19:04:18 +0300

    Revert "защита от двойного рендера renderRootComponent в Web,"
    This reverts commit 9e55c9f95fa39af8d731d15c4c7f99a3c10a4bf2.

--- index.ts
+++ index.ts
@@ -7,31 +7,22 @@ import { Platform } from 'react-native'
 import '@/shared/i18n'
 import '@/shared/styles/themes'
 
-let skiaInitPromise: Promise<void> | null = null
-function initSkiaWeb() {
-	if (skiaInitPromise) return skiaInitPromise
-
-	skiaInitPromise = import('@shopify/react-native-skia/lib/module/web')
-		.then(({ LoadSkiaWeb }) =>
-			LoadSkiaWeb({
-				locateFile: (file: string) => `/${file}`
-			}).then(() => {
-				console.log('Skia Web loaded')
-			})
-		)
-		.then(() => {})
-		.catch((err) => {
-			console.error('Failed to load Skia web:', err)
-		})
-
-	return skiaInitPromise
-}
-
 // Инициализируем Skia только для веб-платформы перед рендером
 if (Platform.OS === 'web') {
-	initSkiaWeb().then(() => {
-		renderRootComponent(App)
-	})
+	import('@shopify/react-native-skia/lib/module/web').then(
+		({ LoadSkiaWeb }) => {
+			LoadSkiaWeb({
+				locateFile: (file: string) => `/${file}` // ищет canvaskit.wasm в public/
+			})
+				.then(() => {
+					renderRootComponent(App)
+				})
+				.catch((err) => {
+					console.error('Failed to load Skia web:', err)
+					renderRootComponent(App)
+				})
+		}
+	)
 } else {
 	renderRootComponent(App)
 }
