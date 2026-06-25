2e723e275feeb9b13724d3808c3f2bab755b8968
webowski <okwebowski@gmail.com>
2026-06-25 16:10:05 +0300

    editorToolbar.store минимальный

--- src/shared/model/editorToolbar.store.ts
+++ src/shared/model/editorToolbar.store.ts
@@ -4,18 +4,10 @@ import type { SubitemId } from '@/shared/domain/ids'
 
 type EditorToolbarStore = {
 	focusedSubitemId: SubitemId | null
-	handleRemove: () => void
-	handleAddSubitem: () => void
+	setFocusedSubitemId: (id: SubitemId | null) => void
 }
 
 export const useEditorToolbarStore = create<EditorToolbarStore>((set) => ({
 	focusedSubitemId: null,
-	setFocusedSubitemId: (id: SubitemId | null) => set({ focusedSubitemId: id }),
-
-	handleRemove: () => {
-		// удаляем subitem c focusedSubitemId
-	},
-	handleAddSubitem: () => {
-		// добавляем subitem
-	}
+	setFocusedSubitemId: (id: SubitemId | null) => set({ focusedSubitemId: id })
 }))
