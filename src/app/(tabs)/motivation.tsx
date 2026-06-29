74f79ce51b0d1fd9430667947d528eb13e2d6fe0
webowski <okwebowski@gmail.com>
2026-06-29 13:49:49 +0300

    MotivationScreen: Replace ScrollBox with KeyboardAwareScrollView and add tap-to-dismiss

--- src/app/(tabs)/motivation.tsx
+++ src/app/(tabs)/motivation.tsx
@@ -1,11 +1,15 @@
 import { useQueryClient } from '@tanstack/react-query'
 import { ActivityIndicator, Text, View } from 'react-native'
+import { Pressable as GesturePressable } from 'react-native-gesture-handler'
+import {
+	KeyboardAwareScrollView,
+	KeyboardController
+} from 'react-native-keyboard-controller'
 
 import { useMotivationSubitems } from '@/features/Motivation'
 import { ChecklistItem } from '@/features/TaskList/ChecklistItem'
 import { supabaseClient } from '@/shared/api/supabaseClient'
-import { commonStyles } from '@/shared/styles/common'
-import ScrollBox from '@/shared/ui/ScrollBox'
+import { commonStyles, staticStyles, STYLE_VARS } from '@/shared/styles/common'
 
 export default function MotivationScreen() {
 	const queryClient = useQueryClient()
@@ -48,15 +52,25 @@ export default function MotivationScreen() {
 		)
 
 	return (
-		<ScrollBox>
-			{subitems?.map((subitem) => (
-				<ChecklistItem
-					key={subitem.id}
-					checked={subitem.type === 'done'}
-					text={subitem.info}
-					onToggle={(_value) => handleToggle(subitem.id, subitem.type)}
-				/>
-			))}
-		</ScrollBox>
+		<KeyboardAwareScrollView
+			style={staticStyles.ScrollBox}
+			overScrollMode='never'
+			bottomOffset={STYLE_VARS.editorToolbarHeight * 1.25}
+		>
+			<GesturePressable
+				style={staticStyles.ScrollBox__inner}
+				onPress={() => KeyboardController.dismiss()}
+				accessibilityRole={undefined}
+			>
+				{subitems?.map((subitem) => (
+					<ChecklistItem
+						key={subitem.id}
+						checked={subitem.type === 'done'}
+						text={subitem.info}
+						onToggle={(_value) => handleToggle(subitem.id, subitem.type)}
+					/>
+				))}
+			</GesturePressable>
+		</KeyboardAwareScrollView>
 	)
 }
