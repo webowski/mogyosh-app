4dbd515019ce4a886b5b6c0b5c52f9449749b20c
webowski <okwebowski@gmail.com>
2026-07-18 02:07:58 +0300

    refactor(ui): replace TaskStatistics with placeholder in progress screen

--- src/app/(tabs)/progress.tsx
+++ src/app/(tabs)/progress.tsx
@@ -1,10 +1,15 @@
-import TaskStatistics from '@/features/Statistics/TaskStatistics'
-import ScrollBox from '@/shared/ui/ScrollBox'
+import { ScrollView, Text } from 'react-native'
+
+import { staticStyles } from '@/shared/styles/common'
 
 export default function ProgressScreen() {
 	return (
-		<ScrollBox>
-			<TaskStatistics />
-		</ScrollBox>
+		<ScrollView
+			style={staticStyles.ScrollBox}
+			contentContainerStyle={staticStyles.ScrollBox__inner}
+			overScrollMode='never'
+		>
+			<Text>Progress</Text>
+		</ScrollView>
 	)
 }
--- src/features/Statistics/TaskStatistics.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native'
-
-export default function TaskStatistics() {
-	return (
-		<View>
-			<Text>Task Statistics</Text>
-		</View>
-	)
-}
