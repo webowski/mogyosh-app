5b351af8b2a2e9bfbe3cf8366945edf94e9c5a99
webowski <okwebowski@gmail.com>
2026-07-19 15:26:50 +0300

    fix: correct paddingLeft calculation in BlockNode styles

--- src/features/Block/BlockNode.tsx
+++ src/features/Block/BlockNode.tsx
@@ -167,7 +167,7 @@ export default function BlockNode({
 
 const styles = StyleSheet.create((theme) => ({
 	indent: (depth: number) => ({
-		paddingLeft: depth * 24
+		paddingLeft: depth ? 24 : 0
 	}),
 	Block: {
 		paddingBottom: 2
