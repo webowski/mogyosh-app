31ddbae6499c09616eb182e7c32f4976191014ae
webowski <okwebowski@gmail.com>
2026-07-30 21:19:24 +0300

    HeaderTask: Добавлен стилизованный бейдж для даты

--- src/features/Header/HeaderTask.tsx
+++ src/features/Header/HeaderTask.tsx
@@ -9,6 +9,7 @@ import { makeCategoryPath } from '@/features/TaskList/model/task.utils'
 import { useCalendarStore } from '@/shared/model/calendar.store'
 import { useTaskStore } from '@/shared/model/task.store'
 import { commonStyles, STYLE_VARS } from '@/shared/styles/common'
+import { StyleSheet } from 'react-native-unistyles'
 
 type HeaderProps = BottomTabHeaderProps | NativeStackHeaderProps
 
@@ -31,23 +32,43 @@ export default function HeaderTask({
 		<View
 			style={[
 				commonStyles.Header,
+				commonStyles.rowBetween,
 				{
 					paddingTop: insets.top + STYLE_VARS.insetPlus
 				}
 			]}
 		>
-			<Text style={commonStyles.headerSubtitle}>
-				{data?.category
-					? makeCategoryPath(data?.category?.id, categoryMap)
-					: ' '}
-			</Text>
-			<Text style={commonStyles.headerTitle}>{!error && data?.title}</Text>
-			<Text style={commonStyles.headerSubtitle}>
-				{selectedDate.toLocaleDateString('ru-RU', {
-					day: 'numeric',
-					month: 'long'
-				})}
-			</Text>
+			<View>
+				<Text style={commonStyles.headerSubtitle}>
+					{data?.category
+						? makeCategoryPath(data?.category?.id, categoryMap)
+						: ' '}
+				</Text>
+				<Text style={commonStyles.headerTitle}>{!error && data?.title}</Text>
+			</View>
+			<View style={styles.HeaderTask__dateBadge}>
+				<Text style={styles.HeaderTask__dateBadgeText}>
+					{selectedDate.toLocaleDateString('ru-RU', {
+						day: 'numeric',
+						month: 'long'
+					})}
+				</Text>
+			</View>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create((theme) => ({
+	HeaderTask__dateBadge: {
+		paddingHorizontal: 10,
+		paddingVertical: 6,
+		borderRadius: 8,
+		backgroundColor: theme.colors.surfaceDeep
+	},
+	HeaderTask__dateBadgeText: {
+		fontSize: 12,
+		fontWeight: 500,
+		lineHeight: 12 * 1.2,
+		color: theme.colors.mutedTextStrong
+	}
+}))
