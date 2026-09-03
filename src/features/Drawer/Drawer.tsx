934ceea017c0722c8c8828b1fade04d9e456d4e7
webowski <okwebowski@gmail.com>
2026-09-03 16:36:08 +0300

    Drawer: Увеличено расстояние между элементами меню с 12 до 14.

--- src/features/Drawer/Drawer.tsx
+++ src/features/Drawer/Drawer.tsx
@@ -238,7 +238,7 @@ const styles = StyleSheet.create((theme, rt) => ({
 		paddingTop: 40 + rt.insets.top,
 		paddingBottom: 40 + 20 + rt.insets.bottom,
 		paddingHorizontal: '16%',
-		gap: 12
+		gap: 14
 	},
 	menuItem: {
 		paddingVertical: 4,
