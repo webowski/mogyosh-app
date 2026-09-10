1c8e4ee7f05b1b5c2e935a060cf1d604f67147e2
webowski <okwebowski@gmail.com>
2026-09-10 16:03:24 +0300

    Button: Изменена прозрачность неактивной кнопки с 0.35 на 0.5.

--- src/shared/ui/Button.tsx
+++ src/shared/ui/Button.tsx
@@ -138,7 +138,7 @@ const styles = StyleSheet.create((theme) => ({
 		boxShadow: theme.colors.shadeButton
 	},
 	disabled: {
-		opacity: 0.35
+		opacity: 0.5
 	},
 	text: {
 		includeFontPadding: false
